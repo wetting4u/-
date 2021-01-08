caf723fd46e7b57a84c8de962bf81e1b9cb0a9d4
致聖 林 <wettingforyou@gmail.com>
2021-01-09 01:42:55 +0800

    correct

--- 部落格實作/jQuery/Article/Article.js
+++ 部落格實作/jQuery/Article/Article.js
@@ -3,7 +3,7 @@ $(function () {
     //綁定新增文章按鈕點擊事件
     //因為新增文章按鈕是後來用AJAX取回產生，故採用$(document).delegate
     $(document).delegate('#EditArticleModal #editBtn', 'click', function () {
-        $('EditArticleModal form').submit();
+        $('#EditArticleModal form').submit();
     });
 
     //當按下新增留言0.2秒後重整頁面
--- 部落格實作/jQuery/Article/ArticleList.js
+++ 部落格實作/jQuery/Article/ArticleList.js
@@ -3,6 +3,6 @@ $(function () {
     //綁定新增文章按鈕點擊事件
     //因為新增文章按鈕是後來用AJAX取回產生，故採用$(document).delegate
     $(document).delegate('#CreateArticleModal #createBtn', 'click', function (){
-        $('CreateArticleModal form').submit();
+        $('#CreateArticleModal form').submit();
     });
 });
